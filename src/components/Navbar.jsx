7e18f61beec91baa654e1dc0acd2c445992153db
Cjose98 <colquejose53@gmail.com>
2025-05-03 20:16:47 -0300

    +cambios en navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo_j} alt='logo' className='w-9 h-9 object-contain' />
+          {/*<img src={logo_j} alt='logo' className='w-9 h-9 object-contain' />*/}
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Jose &nbsp;
             <span className='sm:block hidden'> | Developer</span>
